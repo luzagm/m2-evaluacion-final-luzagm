f595a5057c3e46734e889640de4eef00d20cc7a6
luzagm <luz.aguirre.mo@gmail.com>
2019-08-09 09:55:30 +0200

    add changes in css

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -16,22 +16,25 @@ function searchShows(event) {
   fetch(url) //Busco en la API los datos que necesito
     .then(response => response.json())
     .then(data => {
+      // data = formatData(data);
       shows = data;
-      console.log(shows);
-      let searchResult = "";
 
-      let showImage = "";
+      let searchResult = "";
 
       for (let index = 0; index < shows.length; index++) {
         const showName = shows[index].show.name;
 
         if (shows[index].show.image === null) {
-          showImage = `https://via.placeholder.com/210x295/ffffff/666666/?text=TV`;
+          shows[
+            index
+          ].show.image = `https://via.placeholder.com/210x295/ffffff/666666/?text=TV`;
         } else {
-          showImage = `${shows[index].show.image.medium}`;
+          shows[index].show.image = shows[index].show.image.medium;
         }
 
-        searchResult += `<div class="js-show-list" data-index='${index}'><p> ${showName} </p><img src="${showImage}"></div>`;
+        searchResult += `<div class="js-show-list" data-index='${index}'><p class="show-list-title"> ${showName} </p><img src="${
+          shows[index].show.image
+        }"></div>`;
       }
 
       originalListShow.innerHTML = searchResult;
@@ -39,8 +42,9 @@ function searchShows(event) {
       const originalShows = document.querySelectorAll(".js-show-list");
       for (const item of originalShows) {
         item.addEventListener("click", selectFav);
+        item.addEventListener("click", paintFavs);
+        // Pinto data (name y image) en el HTML
       }
-      // Pinto data (name y image) en el HTML
     });
 }
 
@@ -52,11 +56,30 @@ function selectFav(ev) {
 }
 
 function paintFavs() {
+  favListShow.innerHTML = "";
   for (const item of favArr) {
-    favListShow.innerHTML = `<li>${item.show.name} <img src="${
+    favListShow.innerHTML += `<li><div class="top-title-fav">${
+      item.show.name
+    } <div class="delete">X</div></div> <img src="${
       item.show.image
-    }"></li>`;
+    }" class="js-fav-image"></li>`;
   }
 }
+
+// function formatData(data) {
+//   const result = [];
+//   for (const show of data) {
+//     result.push({
+//       name: show.name,
+//       image: show.image,
+//       id: show.id
+//     });
+//   }
+//   return result;
+// }
+
+// const saveDataInShows = data => {
+//   shows = data;
+// };
 
 button.addEventListener("click", searchShows);
